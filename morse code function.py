ec01d3f780330288ae894d10abe631d595546917
Ziad Hany Mohamed Abdelalim <za2200213@tkh.edu.eg>
2023-11-30 13:10:53 +0200

    Create morse code function.py
    add dictionary

---
+++ morse code function.py
@@ -0,0 +1,11 @@
+# Define a custom exception class for Morse code errors
+class MorseCodeError(Exception):
+    pass
+
+def english_to_morse(text):
+    # Dictionary mapping English characters to Morse code
+    encrypt = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', ' ': '/'
+    }
