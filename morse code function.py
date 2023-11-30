6fee5f363735c1de54ec6d9bf8e5180916ca2788
Ziad Hany Mohamed Abdelalim <za2200213@tkh.edu.eg>
2023-11-30 13:15:26 +0200

    Update morse code function.py
    english to morse

--- morse code function.py
+++ morse code function.py
@@ -9,3 +9,12 @@ def english_to_morse(text):
         'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
         'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', ' ': '/'
     }
+
+    morse_text = []  # Placeholder for storing Morse code text
+    for char in text:
+        # Check if the character is not a valid English character or space
+        if char.upper() not in encrypt and char != ' ':
+            # Raise an error if the character is not in the Morse code dictionary
+            raise MorseCodeError(f"Error: Character '{char}' is not in the Morse code dictionary.")
+        # Convert character to Morse code and append to the list
+        morse_text.append(encrypt.get(char.upper(), ''))  
